1765836a94440ce654adc3a2193cb571d9d063b4
zhonglh <493843721@qq.com>
2018-07-27 19:04:35 +0800

    在 接口上也加了 泛型 ， 否则报错 id 对应的jdbc null

--- module-bms-core/src/main/java/com/zz/bms/core/db/mybatis/query/Query.java
+++ module-bms-core/src/main/java/com/zz/bms/core/db/mybatis/query/Query.java
@@ -6,10 +6,26 @@ import java.io.Serializable;
 
 public interface Query<M ,PK extends Serializable> {
 
+    /**
+     * Query 转 Wrapper
+     * @return
+     */
     public  Wrapper<M> buildWrapper() ;
 
+    /**
+     * Query 转 Wrapper , 在 参数w的基础上
+     * @param w
+     * @return
+     */
     public  Wrapper<M> buildWrapper(Wrapper w) ;
 
 
-    public Wrapper<M> buildWrapper(Wrapper wrapper , boolean orBoolean) ;
+    /**
+     * Query 转 Wrapper , 在 参数w的基础上
+     * 多个条件是否为or的关系
+     * @param w
+     * @param orBoolean    是否为OR 的关系
+     * @return
+     */
+    public Wrapper<M> buildWrapper(Wrapper w , boolean orBoolean) ;
 }
