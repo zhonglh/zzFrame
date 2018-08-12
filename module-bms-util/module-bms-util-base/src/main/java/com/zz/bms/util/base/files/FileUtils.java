a8bb08f9b26a53b692a4cb9aaa7fae94ee928cf9
zhonglh <493843721@qq.com>
2018-08-12 20:56:53 +0800

    修改 更新信息 逻辑BUG

--- module-bms-util/module-bms-util-base/src/main/java/com/zz/bms/util/base/files/FileUtils.java
+++ module-bms-util/module-bms-util-base/src/main/java/com/zz/bms/util/base/files/FileUtils.java
@@ -7,6 +7,7 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.List;
 
@@ -181,4 +182,34 @@ public class FileUtils {
 
 		return fileDelete.delete();
 	}
+
+
+
+	public static void download(HttpServletRequest request, HttpServletResponse response, String storeName,
+								String contentType, String realName) throws Exception {
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+
+		String downLoadPath = storeName;
+
+		long fileLength = new File(downLoadPath).length();
+
+		response.setContentType(contentType);
+		response.setHeader("Content-disposition",
+				"attachment; filename=" + new String(realName.getBytes("utf-8"), "ISO8859-1"));
+		response.setHeader("Content-Length", String.valueOf(fileLength));
+
+		bis = new BufferedInputStream(new FileInputStream(downLoadPath));
+		bos = new BufferedOutputStream(response.getOutputStream());
+		byte[] buff = new byte[2048];
+		int bytesRead;
+		while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+			bos.write(buff, 0, bytesRead);
+		}
+		bis.close();
+		bos.close();
+	}
+
 }
